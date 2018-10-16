c26146cf16b3e3923ac8a5361eb28c37cd633517
unknown <dinghaoyi@gameabc.corp>
2018-10-16 18:30:58 +0800

    some files

--- AU3/ftw.au3
+++ AU3/ftw.au3
@@ -1,6 +1,9 @@
 #include <HotString.au3>
 HotKeySet("^g", googleSearch)
+
 HotStringSet("/g{ENTER}", GoogleSearch)
+HotStringSet("/b{ENTER}", BaiduSearch)
+HotStringSet("/yd{ENTER}", YouDao)
 Global $sOldClip
 
 While 1
@@ -11,5 +14,19 @@ Func GoogleSearch()
 $sOldClip = ClipGet()
 Send("^c")
 ShellExecute("https://www.google.com/search?q=" & ClipGet())
+ClipPut($sOldClip)
+EndFunc
+
+Func BaiduSearch()
+$sOldClip = ClipGet()
+Send("^c")
+ShellExecute("https://www.baidu.com/s?wd=" & ClipGet())
+ClipPut($sOldClip)
+EndFunc
+
+Func YouDao()
+$sOldClip = ClipGet()
+Send("^c")
+ShellExecute("https://dict.youdao.com/w/eng/" & ClipGet())
 ClipPut($sOldClip)
 EndFunc
---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,103 @@
+New-Alias -Name python2 -Value Start-Python2
+New-Alias -Name python3 -Value Start-Python3
+New-Alias -Name pip2 -Value Start-Pip2
+New-Alias -Name pip3 -Value Start-Pip3
+
+function Start-Python2 {
+    py -2 $args
+}
+
+function Start-Python3 {
+    py -3 $args
+}
+
+function Start-Python2 {
+    py -2 $args
+}
+
+function Start-Pip2 {
+   py -2 -m pip $args
+}
+
+function Start-Pip3 {
+   py -3 -m pip $args
+}
+
+# Ensure PSReadLine is loaded
+Import-Module -Name PSReadLine
+
+# Ensure posh-git is loaded
+Import-Module -Name posh-git
+
+# Ensure oh-my-posh is loaded
+Import-Module -Name oh-my-posh
+
+
+
+# Default the prompt to agnoster oh-my-posh theme
+Set-Theme tehrob
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Ensure that Get-ChildItemColor is loaded
+Import-Module Get-ChildItemColor
+
+# Set l and ls alias to use the new Get-ChildItemColor cmdlets
+Set-Alias ll Get-ChildItemColor -Option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
+
+# Helper function to change directory to my development workspace
+# Change c:\ws to your usual workspace and everytime you type
+# in cws from PowerShell it will take you directly there.
+function cws { Set-Location c:\ws }
+
+# Helper function to set location to the User Profile directory
+function cuserprofile { Set-Location ~ }
+Set-Alias ~ cuserprofile -Option AllScope
+
+# Helper function to show Unicode character
+function U
+{
+    param
+    (
+        [int] $Code
+    )
+ 
+    if ((0 -le $Code) -and ($Code -le 0xFFFF))
+    {
+        return [char] $Code
+    }
+ 
+    if ((0x10000 -le $Code) -and ($Code -le 0x10FFFF))
+    {
+        return [char]::ConvertFromUtf32($Code)
+    }
+ 
+    throw "Invalid character code $Code"
+}
+
+
+$SCRIPTPATH = "C:\Program Files (x86)\Vim"
+$VIMPATH    = $SCRIPTPATH + "\vim80\vim.exe"
+
+
+Set-Alias vi   $VIMPATH
+Set-Alias vim  $VIMPATH
+
+
+# for editing your PowerShell profile
+Function Edit-Profile
+{
+    vim $profile
+}
+
+
+# for editing your Vim settings
+Function Edit-Vimrc
+{
+    vim $home\_vimrc
+}
