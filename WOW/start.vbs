188f0b3aa7fccfcc0f2033e48265f7ee10534b4f
Haoyi Ding <dinghaoyi@miluo.xyz>
2019-08-22 16:08:01 +0800

    feat(*): optimize structure and content

--- WOW/start.vbs
+++
@@ -1,8 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell")
-do
-    Randomize
-    WScript.Sleep Int((1000- 500+ 1) * Rnd + 500)
-    WshShell.SendKeys "{END}"
-    WScript.Sleep Int((1000- 500+ 1) * Rnd + 500)
-    WshShell.SendKeys "{HOME}"
-loop
--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,9 +0,0 @@
-New-Alias -Name cnpm -Value Start-Cnpm
-
-function Start-Cnpm {
-    npm --registry=https://registry.npm.taobao.org `
-    --cache=$HOME/.npm/.cache/cnpm `
-    --disturl=https://npm.taobao.org/mirrors/node `
-    --userconfig=$HOME/.cnpmrc `
-    $args
-}
