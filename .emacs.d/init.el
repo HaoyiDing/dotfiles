e4211df67ec93eab9ba87bd179cbd833f6746c6f
Haoyi Ding <dingdingly55@hotmail.com>
2020-12-06 21:25:52 +0800

    add auctex and evil leader

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,21 +2,43 @@
                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize) ;; You might already have this line
 
+;; init evil
 (require 'evil)
 (evil-mode 1)
+
+;; init evil leader
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+(evil-leader/set-key
+ "e" 'find-file
+ "b" 'switch-to-buffer
+ "k" 'kill-buffer
+ "tv" 'TeX-command-run-all)
+
+;; init nord theme
+(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
+(load-theme 'nord t)
+
+;; init auctex
+(load "auctex.el" nil t t)
+;; View my PDFs with Evince 
+(setq TeX-view-program-list '(("Evince" "evince --page-index=%(outpage) %o")))
+(setq TeX-view-program-selection '((output-pdf "Evince")))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(global-linum-mode t)
  '(inhibit-startup-screen t)
- '(package-selected-packages (quote (nord-theme evil))))
+ '(package-selected-packages
+   (quote
+    (company-auctex auctex evil-leader company nord-theme evil))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-
-(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
-(load-theme 'nord t)
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 158 :width normal)))))
