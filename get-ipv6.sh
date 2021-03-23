ee1a0be386bab940a93c359caf7596930adbf219
Haoyi Ding <dingdingly55@hotmail.com>
2021-03-24 07:12:27 +0800

    Update get-ipv6.sh

--- get-ipv6.sh
+++ get-ipv6.sh
@@ -1,7 +1,7 @@
 local hostIP_v6=$(ip addr show pppoe-wan | grep inet6 | sed 's/\/.*//g' | awk '{print $2}'  | grep 2408)
 while [[ -z $hostIP_v6 ]]
   do
-    sleep 0.5
+    sleep 1
     hostIP_v6=$(ip addr show pppoe-wan | grep inet6 | sed 's/\/.*//g' | awk '{print $2}'  | grep 2408)
   done
 echo $hostIP_v6
