e6bf2471351b687a587364cf8500836353157cb8
Haoyi Ding <dingdingly55@hotmail.com>
2021-08-13 16:44:38 +0800

    update nvim configurations

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,40 @@
-require 'core'
+-- Basic
+require 'globals'
+require 'settings'
+require 'keybinds'
+require 'plugins'
 
+-- Theme
+require 'nv-theme'
+-- Status line
+require 'nv-galaxyline'
+-- For hex color
+require 'nv-colorizer'
+-- File explorer
+require 'nv-nvimtree'
+-- For indent line
+require 'nv-indentline'
+-- Float terminal
+require 'nv-floaterm'
+-- Buffer line
+require 'nv-bufferline'
+-- Make finding stuff easier
+require 'nv-telescope'
+-- Neovim LSP
+require 'nv-lspconfig'
+-- Tab/Auto completion
+require 'nv-compe'
+-- For nice highlighing
+require 'nv-treesitter'
+-- For snippets
+require 'nv-vsnip'
+-- Auto pairs '', "", [], {}, (), ``
+require 'nv-autopairs'
+-- vscode-like pictograms for neovim lsp completion items
+require 'nv-lspkind'
+-- Provide git signs
+require 'nv-gitsigns'
+-- Smooth scrolling
+require 'nv-neoscroll'
+-- Show warnings, errors list
+require 'nv-trouble'
---
+++ nvim/lua/globals.lua
@@ -0,0 +1,5 @@
+O = vim.o
+BO = vim.bo
+WO = vim.wo
+G = vim.g
+CMD = vim.cmd
